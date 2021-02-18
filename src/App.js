a67fb0e8b5aecce4dc38785233b08b243dab351e
Petr <musakius.work@gmail.com>
2021-02-18 09:50:22 +0300

    feat: add bootstrap

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Main from './pages/Main';
 import Settings from './pages/Settings';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <>
+    <div className="app">
       <Header />
       <Switch>
         <Route path="/game" render={() => <Main />} />
         <Route path="/" exact render={() => <Settings />} />
       </Switch>
       <Footer />
-    </>
+    </div>
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import { connect } from "react-redux";
-import { toggleIsOpenGame } from '../../redux/actions';
+import {connect} from 'react-redux';
+import {toggleIsOpenGame} from '../../redux/actions';
 import './Header.scss';
 
 const Header = ({toggleIsOpenGame}) => {
   return (
     <header className="header">
-     header
-     <button onClick={toggleIsOpenGame}></button>
+      <nav className="navbar navbar-expand-lg navbar-warning bg-warning d-flex justify-content-between align-items-center">
+        <h1 className="m-0">
+          Tic Tac Toe
+        </h1>
+        <button className="btn btn-secondary my-2 my-sm-0" type="submit" onClick={toggleIsOpenGame}>
+          Menu
+        </button>
+      </nav>
     </header>
   );
 };
