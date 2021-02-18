534f3dda8ee6d1756ea013ec87c7431cd708bb76
Petr <musakius.work@gmail.com>
2021-02-18 11:45:17 +0300

    fix: Footer & Header

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
-/* import { ReactComponent as RSschool } from '~public/rs_school'; */
 import './Footer.scss';
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <span>&#169; 2021</span>
-      <div>
-        footer-user
-      </div>
-      <a href='https://rs.school/js/' target='_blank' rel='noopener noreferrer'>
-        {/* <RSschool /> */}
+      <span className="footer__year">&#169; 2021</span>
+      <a
+        className="link"
+        href="https://github.com/musakius"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className="fab fa-github logoGit"></i>
+        musakius
+      </a>
+      <a className="link" href="https://rs.school/js/" target="_blank" rel="noopener noreferrer">
+        <img className="logoRSS" src="https://rs.school/images/rs_school-og.png" alt="rs_school" />
       </a>
     </footer>
   );
